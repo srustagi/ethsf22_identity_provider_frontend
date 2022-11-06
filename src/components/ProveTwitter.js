9248ececef157931731fea23a53653e54183fb19
Shiv Rustagi <shivrustagi221@gmail.com>
2022-11-05 18:06:26 -0700

    foiled again

--- src/components/ProveTwitter.js
+++ src/components/ProveTwitter.js
@@ -3,6 +3,10 @@ import { useSignMessage, useAccount } from 'wagmi'
 import { verifyMessage } from 'ethers/lib/utils'
 import axios from 'axios';
 import styles from './ProveTwitter.module.css'
+import { Web3Storage, getFilesFromPath } from 'web3.storage'
+import { File } from 'web3.storage'
+
+import addEntry from '../storage/add-entry.js'
 
 let uuidv4 = () => {
   return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
@@ -36,11 +40,8 @@ let ProveTwitter = () => {
     event.preventDefault()
     const formData = new FormData(event.target)
     setTwitterProof(formData.get('tweet_proof'))
-    console.log(twitterProof)
     const tweet_id = /[0-9]{19}/
     const tweet_id_match = twitterProof.match(tweet_id)
-    console.log(tweet_id_match[0])
-    console.log(process.env)
     axios({
       method: 'get',
       url: `https://api.twitter.com/2/tweets/${tweet_id_match[0]}`,
@@ -49,11 +50,21 @@ let ProveTwitter = () => {
         'Authorization': `Bearer AAAAAAAAAAAAAAAAAAAAAM3UiwEAAAAA9q8i8JIMtWXrri9xN%2BtcEKaVJ8A%3DRNyPbmbSiUzTLlEodHWxvnzuQBd4gZZE1h1hx9CksYMiC6zvHz`
       },
     })
-      .then(function (response) {
-        let res = JSON.parse(response.data)
-        let signature_regex = /(?<=Signature:\s)0x[a-zA-Z0-9]+/
-        let regex_res = res.data.text.match(signature_regex)[0]
-        console.log(regex_res)
+      .then(async (response) => {
+        let res = JSON.parse(response.data);
+        let signature_regex = /(?<=Signature:\s)0x[a-zA-Z0-9]+/;
+        let regex_res = res.data.text.match(signature_regex)[0];
+
+        let user = {address: {twitter: '', github: '', polygon: ''}}
+        user.address.twitter = regex_res
+        let user_json = JSON.stringify(user)
+        let files = [new File([Buffer.from(user_json)], `${address}.json`)]
+        let storage = new Web3Storage({ token: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDVkNEQ0ZUIxQzM2MjVEODQ5ODQ4MDcyMDE5RWYyODUxMTYyRjI2M0QiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2Njc2MTcyMDg1OTgsIm5hbWUiOiJldGhzZi10ZXN0In0.L1l9NFwuQMPy19ggA-K7Z-1Youzd7hR9_CnVab_slCE` });
+
+        let cid = await storage.put(files);
+        console.log(cid);
+        console.log('Content added with CID:', cid);
+        // let output = (addEntry(address, cid));
       });
   }
   
@@ -79,7 +90,7 @@ let ProveTwitter = () => {
           <div className={styles.result}>
             <div className={styles.twtproof}>Verifying myself ({recoveredAddress.current}) for Attest with Twitter handle {twitterHandle}. Signature: {data}</div>
             <div>
-              <a attribute="_blank" className={styles.tweet} href={
+              <a target="_blank" className={styles.tweet} href={
                 `https://twitter.com/intent/tweet?text=Verifying myself (${recoveredAddress.current}) for Attest with Twitter handle ${twitterHandle}. Signature: ${data}`
                 }>Tweet This!</a>
             </div>
--- src/storage/put-files.js
+++ src/storage/put-files.js
@@ -2,19 +2,11 @@ import process from 'process'
 import minimist from 'minimist'
 import { Web3Storage, getFilesFromPath } from 'web3.storage'
 import addEntry from './add-entry.js'
+import * as fs from 'fs'
 
 async function main () {
   //TODO: get enviroment variables from process
-  const args = minimist(process.argv.slice(2))
-  const token = args.token
-
-  if (!token) {
-    return console.error('A token is needed. You can create one on https://web3.storage')
-  }
-
-  if (args._.length < 1) {
-    return console.error('Please supply the path to a file or directory')
-  }
+  const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDVkNEQ0ZUIxQzM2MjVEODQ5ODQ4MDcyMDE5RWYyODUxMTYyRjI2M0QiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2Njc2MTcyMDg1OTgsIm5hbWUiOiJldGhzZi10ZXN0In0.L1l9NFwuQMPy19ggA-K7Z-1Youzd7hR9_CnVab_slCE`
 
   const storage = new Web3Storage({ token })
   const files = []
